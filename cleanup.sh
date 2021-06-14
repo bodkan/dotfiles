73bb083a47f393d2e58cb30a72791d8ecc799aa9
Martin Petr <git@bodkan.net>
2021-06-14 13:55:47 +0200

    Fix Emacs cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+cd emacs.d
 rm -rf -v auto-save-list/ bookmarks elpa/ emacs-custom org-clock-save.el recentf tramp transient/ snippets/ elpy/ var/ eln-cache/ projectile-bookmarks.eld network-security.data projectile.cache
