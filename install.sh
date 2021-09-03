8291ed3a9270f55e74ca123ccbece24c64610c43
Martin Petr <git@bodkan.net>
2021-09-03 14:05:19 +0200

    Update Rprofile

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ done
 
 [ ! -d ~/.emacs.d ] && ln -sv `realpath emacs.d/` ${HOME}/.emacs.d
 
+mkdir -p ~/.my_local/R_LIBS
+
 echo "PATH=$PATH" > ~/.Renviron
 echo "R_LIBS_USER=~/.my_local/R_LIBS" >> ~/.Renviron
 echo "RETICULATE_PYTHON=~/.pyenv/versions/retipy/bin/python3" >> ~/.Renviron
