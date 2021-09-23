24f0d499474de7d5b98b742ddd0ba0ceb2016544
Martin Petr <git@bodkan.net>
2021-09-23 09:11:52 +0200

    Add comments to the installation script

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# create symlinks for all files under `./dot`
 for f in dot/*; do
     ln -sv `realpath $f` ~/.`basename $f`
 done
 
+# link the entire ~/.emacs.d/ config directory
 [ ! -d ~/.emacs.d ] && ln -sv `realpath emacs.d/` ${HOME}/.emacs.d
 
 mkdir ~/.my_local
 mkdir ~/.my_local/bin
 mkdir ~/.my_local/R_LIBS
 
+# generate ~/.Renviron file with necessary configs
 echo "PATH=$PATH" > ~/.Renviron
 echo "R_LIBS_USER=~/.my_local/R_LIBS" >> ~/.Renviron
 echo "R_BUILD_TAR=tar" >> ~/.Renviron
