60f307cc54682cb641227d0204dab03239e2369e
Martin Petr <git@bodkan.net>
2024-02-29 11:28:44 +0100

    Update dotfiles

--- install.sh
+++ install.sh
@@ -13,9 +13,6 @@ for f in bin/*; do
 done
 
 
-mkdir ~/.ssh
-cp racimolab.txt ~/.ssh
-
 mkdir -p ~/.config/rstudio/keybindings
 ln -sv `realpath rstudio-prefs.json` ~/.config/rstudio/rstudio-prefs.json
 ln -sv `realpath rstudio_bindings.json` ~/.config/rstudio/keybindings/rstudio_bindings.json
@@ -28,11 +25,9 @@ fi
 
 # generate ~/.Renviron file with necessary configs
 echo "PATH=$PATH" > ~/.Renviron
-#if [[ "$OSTYPE" == "darwin"* ]]; then
-#    mkdir -p ~/.my_local/R_LIBS
-#    echo "R_LIBS_USER=~/.my_local/R_LIBS" >> ~/.Renviron
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    mkdir -p ~/.my_local/R_LIBS
+    echo "R_LIBS_USER=~/.my_local/R_LIBS" >> ~/.Renviron
 if [ -z "$IN_DOCKER" ]; then
     echo "R_LIBS_USER=~/projects/.R_LIBS" >> ~/.Renviron
 fi
-#echo "R_BUILD_TAR=tar" >> ~/.Renviron
-
