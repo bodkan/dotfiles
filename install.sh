37d8e02b6fc0b298aca589e567d2d9528b16c892
Martin Petr <git@bodkan.net>
2025-03-09 14:19:02 +0100

    Update config

--- install.sh
+++ install.sh
@@ -2,33 +2,33 @@
 
 # create symlinks for all files under `./dot`
 for f in dot/*; do
-    ln -sv `realpath $f` ~/.`basename $f`
+    ln -sv `realpath $f` $HOME/.`basename $f`
 done
 
-mkdir ~/.my_local
-mkdir ~/.my_local/bin
+mkdir $HOME/.my_local
+mkdir $HOME/.my_local/bin
 
 for f in bin/*; do
-    ln -sv `realpath $f` ~/.my_local/bin/`basename $f`
+    ln -sv `realpath $f` $HOME/.my_local/bin/`basename $f`
 done
 
-mkdir -p ~/.config/rstudio/keybindings
-ln -sv `realpath rstudio-prefs.json` ~/.config/rstudio/rstudio-prefs.json
-ln -sv `realpath rstudio_bindings.json` ~/.config/rstudio/keybindings/rstudio_bindings.json
+mkdir -p $HOME/.config/rstudio/keybindings
+ln -sv `realpath rstudio-prefs.json` $HOME/.config/rstudio/rstudio-prefs.json
+ln -sv `realpath rstudio_bindings.json` $HOME/.config/rstudio/keybindings/rstudio_bindings.json
 
 # generate ~/.Renviron file with necessary configs
-echo "PATH=$PATH" > ~/.Renviron
+echo "PATH=$PATH" > $HOME/.Renviron
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    mkdir -p ~/.my_local/R_LIBS
-    echo "R_LIBS_USER=~/.my_local/R_LIBS" >> ~/.Renviron
-elif [[ -z "$IN_DOCKER" ]]; then
-    echo "R_LIBS_USER=~/projects/.R_LIBS" >> ~/.Renviron
+    mkdir -p $HOME/.my_local/R_LIBS
+    echo "R_LIBS_USER=$HOME/.my_local/R_LIBS" >> $HOME/.Renviron
+elif [[ ! -f /.dockerenv ]]; then
+    echo "R_LIBS_USER=$HOME/projects/.R_LIBS" >> $HOME/.Renviron
 fi
 
-# save GitHub access token if present
-if [[ -f ~/.Renviron ]]; then
-    GITHUB_PAT=$(awk -F= '/GITHUB_PAT/{print $2}' ~/.Renviron)
-    echo GITHUB_PAT=$GITHUB_PAT >> ~/.Renviron
+# keep around GitHub access token if present
+if [[ -f $HOME/.Renviron ]]; then
+    GITHUB_PAT=$(awk -F= '/GITHUB_PAT/{print $2}' $HOME/.Renviron)
+    echo GITHUB_PAT=$GITHUB_PAT >> $HOME/.Renviron
 fi
 
 # install vim plugins
@@ -38,7 +38,6 @@ cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/surround.git
 vim -u NONE -c "helptags surround/doc" -c q
 
-# vim-gitgutter
 mkdir -p ~/.vim/pack/airblade/start
 cd ~/.vim/pack/airblade/start
 git clone https://github.com/airblade/vim-gitgutter.git
