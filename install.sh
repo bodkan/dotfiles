22e3579541246195bcf0cd48876c61ed872b7f5c
Martin Petr <git@bodkan.net>
2025-10-31 16:40:24 +0100

    Update dotfiles after Tahoe upgrade

--- install.sh
+++ install.sh
@@ -14,10 +14,10 @@ done
 
 # a couple of binaries and scripts
 echo "Creating symlinks to binaries and scripts..."
-mkdir -p $HOME/.my_local
-mkdir -p $HOME/.my_local/bin
+mkdir -p $HOME/.mylocal
+mkdir -p $HOME/.mylocal/bin
 for f in bin/*; do
-    ln -s `realpath $f` $HOME/.my_local/bin/`basename $f`
+    ln -s `realpath $f` $HOME/.mylocal/bin/`basename $f`
 done
 
 # generate ~/.Renviron file with necessary contents
@@ -28,8 +28,8 @@ GITHUB_PAT=$(awk -F= '/GITHUB_PAT/{print $2}' $HOME/.Renviron)
 
 echo "PATH=$PATH" > $HOME/.Renviron
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    mkdir -p $HOME/.my_local/R_LIBS
-    echo "R_LIBS_USER=$HOME/.my_local/R_LIBS" >> $HOME/.Renviron
+    mkdir -p $HOME/.mylocal/R_LIBS
+    echo "R_LIBS_USER=$HOME/.mylocal/R_LIBS" >> $HOME/.Renviron
 elif [[ ! -f /.dockerenv ]]; then
     echo "R_LIBS_USER=$HOME/projects/.R_LIBS" >> $HOME/.Renviron
 fi
