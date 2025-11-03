88210fba354e8665c41a82278f34f13c73faf0eb
Martin Petr <git@bodkan.net>
2025-11-03 16:41:43 +0100

    Remove GITHUB_PAT

--- install.sh
+++ install.sh
@@ -31,12 +31,3 @@ echo "PATH=$PATH" > $HOME/.Renviron
 if [[ ! -f /.dockerenv ]] && [[ "$OSTYPE" != "darwin"* ]]; then
     echo "R_LIBS_USER=$HOME/projects/.R_LIBS" >> $HOME/.Renviron
 fi
-
-# keep around GitHub access token if present
-if [[ -n $GITHUB_PAT ]]; then
-    echo "Generating GitHub access token..."
-    echo GITHUB_PAT=$GITHUB_PAT >> $HOME/.Renviron
-else
-    echo "Skipping GitHub access token..."
-fi
-
